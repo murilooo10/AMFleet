4a99acbf79ab3744e7cd8411cec5a7327d6727b7
Augusto Gabillan Filho <augustogabillanfilho@icloud.com>
2020-09-03 23:42:17 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ import { NavigationContainer} from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './pages/home';
 import Veiculos from './pages/veiculos';
+import Motoristas from './pages/motoristas'
+import Comprovantes from './pages/comprovantes'
+import Pecas from './pages/pecas'
 import Login from './pages/login';
 
 const AppStack = createStackNavigator();
@@ -13,6 +16,10 @@ export default function Routes(){
             <AppStack.Navigator screenOptions={{headerShown: false}}>
                 <AppStack.Screen name="Home" component={Home} />
                 <AppStack.Screen name="Veiculos" component={Veiculos} />
+                <AppStack.Screen name="Motoristas" component={Motoristas} />
+				<AppStack.Screen name="Comprovantes" component={Comprovantes} />
+				<AppStack.Screen name="Pecas" component={Pecas} />
+				
             </AppStack.Navigator>
 
         </NavigationContainer>
