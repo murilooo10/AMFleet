0fad0019dda83c4ccff0b89446a16f36a42beb07
Augusto Gabillan Filho <augustogabillanfilho@icloud.com>
2020-09-14 18:26:22 -0300

    Update index.js

--- src/pages/agendamento/index.js
+++ src/pages/agendamento/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Feather} from '@expo/vector-icons';
-import {View, FlatList, Alert, Image, Modal, Text, TouchableOpacity, TouchableHighlight} from 'react-native';
+import {View, FlatList, Alert, Image, Modal, Text, Picker, pickerStyle, TouchableOpacity, TouchableHighlight} from 'react-native';
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 import { Searchbar } from 'react-native-paper';
@@ -14,22 +14,20 @@ import firebase from 'firebase';
 import _ from 'lodash';
 
 
-
 export default class Agendamento extends Component{
     constructor(props){
         super(props)
         this.state = {
             isLogged: false,
-            matricula: "",
-            nome: '',
-            sobrenome: '',
-            email: '',
-            senha: '',
-            confirmarSenha: '',
+            data_inicio: '',
+            pecas:'',
+            status:'',
+            veiculo:'',
             modalVisible: false,
             errorMessage: null,
             array:[],
         }; 
+        this.singOutAccount = this.singOutAccount.bind(this);
     }
 
     //      firebase.database().ref('usuario/' + uid).orderByChild('uid/', (data) => {
@@ -43,9 +41,69 @@ export default class Agendamento extends Component{
     //    this.props.navigation.navigate('Something');
     //}
 
+    componentDidMount = () => {
+        firebase.auth().onAuthStateChanged(function(user){
+            
+            if(user){
+                this.setState({
+                    isAuthenticated: true,
+                })
+                this.getInformation();
+            }
+            else{
+                this.setState({
+                    isAuthenticated: false,
+                })
+                this.navigateToLogin();
+
+            }
+        }.bind(this)
+        );
+    }
+
+    guidGenerator() {
+        var S4 = function() {
+           return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+        };
+        return (S4()+S4()+S4()+S4()+S4()+S4());
+    }
+
+    cadastrarAgendamento = () =>{
+        var uid = this.guidGenerator();
+        const {data_inicio, pecas, status, veiculo, email, password} = this.state;
+        if(data_inicio != null && pecas != null && status != null && veiculo != null){
+            firebase.database().ref(`agendamento/${uid}`).set({
+                data_inicio: this.state.data_inicio,
+                pecas: this.state.pecas,
+                status: this.state.status,
+                veiculo: this.state.veiculo
+            })
+
+        }else{
+            this.setState({errorMessage: "Preencha todos os campos presentes!"});
+        }
+    }
     
+    singOutAccount = () =>{
+        firebase.auth().signOut().then(() =>{
+            this.setState({
+                isAuthenticated:false,
+            })
+            console.log('saiu')
+            this.props.navigation.navigate('Login');
+        }).catch(error =>
+            console.log(error.code))
+    }
+
+
+    navigateToDetailsAgendamento = () =>{
+        this.props.navigation.navigate('DetailsAgendamento');
+    }
+
+
     render(){
         return(
+            
             <View style={styles.container} >
                 <View style={styles.header}>
                     <Image source={logoImg} />
@@ -56,54 +114,13 @@ export default class Agendamento extends Component{
                     <FontAwesome name="power-off" size={24} color="red" />
                     </TouchableRipple>
                 </View>
+                               
                 
-               <Modal
-                animationType='slide'
-                transparent={true}
-                visible={this.state.modalVisible}
-                onRequestClose={() => {
-                  Alert.alert("Modal has been closed.");
-                }}>
-                    <View style={styles.modalView}>
-                        <TouchableRipple
-                            style={styles.alinharClose}
-                            rippleColor="#E9EEF3"
-                            onPress={() => {
-                                this.setState({modalVisible:false})
-                            }}
-                            >
-                            <AntDesign name="close" size={20} color="#D3D3D3" />
-                        </TouchableRipple>
-                        <View style={styles.header}>
-                            <Text style={styles.description}>Cadastre um Motorista</Text>
-                        </View>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="Digite o nome"
-                                value={this.state.nome}
-                                onChangeText={nome=> this.setState({nome})}
-                            />
-         
-                            <View style={styles.errorMessage}>
-                                {this.state.errorMessage && <Text style={styles.wrongText}>{this.state.errorMessage}</Text>}
-                            </View>
-                            <TouchableRipple 
-                                style={styles.button}
-                                rippleColor="#E9EEF3"
-                                onPress={this.cadastrarMotorista}
-                            >
-                                <View>
-                                <Text style={styles.textStyle}>Cadastrar</Text>
-                                </View>
-                            </TouchableRipple>
-                    </View>
-                </Modal>
-                
-
-                <Text style={styles.vehicleProperty}>                   Agendemento de manutenção </Text>
+                <Text style={styles.agdendamentoProperty}>Agendemento de manutenção </Text>
                 <Text style={styles.vehicleProperty}></Text>
                 <Text style={styles.vehicleProperty}></Text>  
                 <Text style={styles.vehicleProperty}>Selecione a marca do veículo: </Text>
+            
                 <RNPickerSelect
                     onValueChange={(value) => console.log(value)}
                     items={[
@@ -111,8 +128,10 @@ export default class Agendamento extends Component{
                         { label: 'Chevrolet', value: 'chevrolet' },
                         { label: 'Renault', value: 'renault' },
                     ]}
+                    
                 />
 
+    
                 <Text style={styles.vehicleProperty}></Text>
                 <Text style={styles.vehicleProperty}></Text>  
                 <Text style={styles.vehicleProperty}>Selecione o modelo do veículo: </Text>
@@ -139,18 +158,28 @@ export default class Agendamento extends Component{
 
                 <Text style={styles.vehicleProperty}></Text>
                 <Text style={styles.vehicleProperty}></Text>
-                <Text style={styles.vehicleProperty}>                    O que é recomendado trocar</Text> 
-                <Text style={styles.vehicleValue}>                        Pastilhas de freio</Text>
+                <Text style={styles.pecaProperty}>O que é recomendado trocar</Text> 
+                <Text style={styles.vehicleValue}>Pastilhas de freio</Text>
              
-
-    
-                <Text style={styles.vehicleProperty}>Selecione as peças que deseja trocar: </Text>
+                <Text style={styles.vehicleProperty}>Selecione o que deseja trocar: </Text>
                 <RNPickerSelect
                     onValueChange={(value) => console.log(value)}
                     items={[
                         { label: 'Pastilhas', value: 'pastilhas' },
                         { label: 'Óleo do motor', value: 'oleo do motor' },
                         { label: 'Filtro de ar', value: 'filtro de ar' },
+                    ]}
+                />  
+
+                <Text style={styles.vehicleProperty}></Text>
+                <Text style={styles.vehicleProperty}></Text>
+                <Text style={styles.vehicleProperty}>Escolha a data para a manutenção: </Text>
+                <RNPickerSelect
+                    onValueChange={(value) => console.log(value)}
+                    items={[
+                        { label: '14/09/2020', value: '14/09/2020' },
+                        { label: '15/09/2020', value: '15/09/2020' },
+                        { label: '16/09/2020', value: '16/09/2020' },
                     ]}
                 />  
                
@@ -162,13 +191,12 @@ export default class Agendamento extends Component{
                 <TouchableOpacity 
                     style={styles.detailsButtonAdd} 
                     rippleColor="#E9EEF3"
-                    onPress={() => {
-                    this.setState({modalVisible: true});
-                    }}
+                    onPress={this.cadastrarAgendamento}
                 >
-                    <AntDesign name="calendar" size={24} color="#00cc00" />
+                    <AntDesign name="calendar" size={36} color="#00cc00" />
                     <Text style={{color:'#00cc00', fontWeight:'bold'}}>Agendar</Text>
                 </TouchableOpacity>
+
                 </View>
             </View>
         )}
